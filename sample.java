4f07cab6770c316676867e2a95c3dc2c8324df85
Ratish Kumar Saravanan <ratishkumarsaravanan@Ratishs-Air-4.lan>
2024-09-02 01:02:24 -0400

    Stack-2 done

--- sample.java
+++ sample.java
@@ -5,3 +5,55 @@
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        Stack<Integer> stack=new Stack<>();
+        int cur=0;
+        int prev=0;
+        for(String l:logs){
+            String[] str=l.split(":");
+            int preTask=Integer.parseInt(str[0]);
+            cur=Integer.parseInt(str[2]);
+            if(str[1].equals("start")){
+                if(!stack.isEmpty()){
+                    res[stack.peek()]+=cur-prev;
+                }
+                stack.push(preTask);
+                prev=cur;
+            }
+            else{
+                res[stack.pop()]+=cur+1-prev;
+                prev=cur+1;
+            }
+        }
+        return res;
+    }
+}
+// T.C=O(n)
+// S.C=O(n)
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] res=new int[n];
+        for(int i=0;i<n;i++)
+            res[i]=1;
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+                res[i]=res[i-1]+1;
+        }
+        
+        int total=res[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+                res[i]=Math.max(res[i],res[i+1]+1);
+        total+=res[i];
+        }
+        return total;
+    }
+}
+// T.C=O(2n)
+// S.C=O(n)
